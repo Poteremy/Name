8fca236f313402f64ac8d5fa6724c8fb9d5cd910
Poteremy <68087635+Poteremy@users.noreply.github.com>
2020-07-25 19:04:50 -0400

    new problem

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ let name = document.getElementById('name');
 let submit = document.getElementById('submit');
 let nameDisplay = document.getElementById('nameDisplay');
 let random = document.getElementById('random');
+let kidsList = document.getElementById('kidsList');
+let i 
 
 test = (e) => {
+    i=0
     e.preventDefault();
-    arr.push(name.value);
+    let obj = ({"text": name.value, "id": i})
+    arr.push(obj);
+    let create = document.createElement('li');
+    create.setAttribute('class', obj['text']);
+    kidsList.appendChild(create);
+    create.innerText = obj.text;
     name.value='';
+    i++;
 }
+
 
 name.addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
@@ -18,24 +28,35 @@ name.addEventListener('keyup', function(event) {
     }
 })
 
+i=0
+
 function pickRandom () {
+
     let ran = Math.floor(Math.random() * arr.length);
     let ranName = arr[ran];
-    nameDisplay.textContent = ranName;
-    let rem = arr.indexOf(ranName)
-    arr.splice(rem, 1)
-    console.log(arr)
+    let listItems = document.querySelectorAll('li')
 
-// Goal here: only call each name once until all names have been called, then reset
-
-    // if(array.length === 0) {
-    //     something to reset it. Tried creating a new array from the original array and resetting the value that way. 
-    //     Succinctly, how to "undo" a splice. Or is there a better way to do that
-        
-    // } else {
-    //     something
-    // }
+     if(i<=arr.length) {
+        for( list of listItems) {
+            listItems.forEach(list => {
+            if(list.classList.contains('picked')) {
+                return
+            } else {
+                nameDisplay.textContent = ranName['text']
+                if(list.classList == nameDisplay.textContent) {
+                    list.classList.add('picked')
+                } else {
+                    return} 
+                }}    
+            )}
+        } else {
+            i=0
+            listItems.forEach(list => {
+                list.classList.remove('picked')
+            })
+            nameDisplay.textContent=''
+        }
+        i++
 }
 
 random.addEventListener('click', pickRandom);
-
