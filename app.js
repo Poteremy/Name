2b49ff0ac65cbf7d124c7aa6a7abeab41a57dd1d
Poteremy <68087635+Poteremy@users.noreply.github.com>
2020-07-25 15:12:50 -0400

    new changes with a question

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 
 let arr = [];
 
+
 let name = document.getElementById('name');
 let submit = document.getElementById('submit');
 let nameDisplay = document.getElementById('nameDisplay');
 let random = document.getElementById('random');
 
-submit.addEventListener('mouseup', function(e){
-    e.preventDefault
-    arr.push(name.value)
-    name.value=''
+test = (e) => {
+    e.preventDefault();
+    arr.push(name.value);
+    name.value='';
+}
+
+name.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        submit.click()
+    }
 })
 
 function pickRandom () {
-    let ran = Math.floor(Math.random() * arr.length)
-    let ranName = arr[ran]
-    nameDisplay.textContent = ranName
-
+    let ran = Math.floor(Math.random() * arr.length);
+    let ranName = arr[ran];
+    nameDisplay.textContent = ranName;
+    let rem = arr.indexOf(ranName)
+    arr.splice(rem, 1)
+    console.log(arr)
+    // if(array.length === 0) {
+    //     // array.push(arr)
+        
+    // } else {
+    //     return
+    // }
 }
 
-random.addEventListener('click', pickRandom)
+random.addEventListener('click', pickRandom);
+
